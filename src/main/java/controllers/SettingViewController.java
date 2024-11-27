8072f1f7b536bad21df27dc319f3768d5af85461
hwHoai <daohuuhoai2655@gmail.com>
2024-11-27 23:33:47 +0700

    Hoai | done setting-view

--- src/main/java/controllers/SettingViewController.java
+++ src/main/java/controllers/SettingViewController.java
@@ -2,21 +2,31 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import services.BackwardScreen;
 import views.SettingMenu;
 
+import java.beans.EventHandler;
 
-public class SettingViewController {
+
+public class SettingViewController implements BackwardScreen {
+    private Stage stage;
+    public Slider musicSlider;
+    public Slider soundSlider;
     @FXML
     private ImageView backBtnIcon;
     @FXML
     private ImageView musicIcon;
     @FXML
     private ImageView soundIcon;
-    @FXML
-    private Label tittle;
+//    @FXML
+//    private Slider musicSlider;
 
     public void view(Stage stage) {
             SettingMenu settingMenu = new SettingMenu();
@@ -29,4 +39,27 @@ public class SettingViewController {
         musicIcon.setImage(new Image(getClass().getResource("/assets/icon/music-setting.png").toExternalForm()));
         soundIcon.setImage(new Image(getClass().getResource("/assets/icon/sound.png").toExternalForm()));
     }
+
+
+    public void onMusicSliderChange(MouseEvent mouseEvent) {
+        if(musicSlider.getValue() == 0) {
+            musicIcon.setImage(new Image(getClass().getResource("/assets/icon/music-off.png").toExternalForm()));
+        } else {
+            musicIcon.setImage(new Image(getClass().getResource("/assets/icon/music-setting.png").toExternalForm()));
+        }
+    }
+
+    public void onSoundSliderChange(MouseEvent mouseEvent) {
+        if(soundSlider.getValue() == 0) {
+            soundIcon.setImage(new Image(getClass().getResource("/assets/icon/sound-mute.png").toExternalForm()));
+        } else {
+            soundIcon.setImage(new Image(getClass().getResource("/assets/icon/sound.png").toExternalForm()));
+        }
+    }
+
+    @Override
+    public void backToMainScreen() throws Exception {
+        ViewController.getMainView();
+    }
 }
+
---
+++ src/main/java/services/BackwardScreen.java
@@ -0,0 +1,5 @@
+package services;
+
+public interface BackwardScreen {
+    void backToMainScreen() throws Exception;
+}
