fff349179387dc61137d6149dad0b86c0924903a
hwHoai <daohuuhoai2655@gmail.com>
2024-12-02 09:37:49 +0700

    Hoai | re-size playing space

--- src/main/java/controllers/PlayingViewController.java
+++ src/main/java/controllers/PlayingViewController.java
@@ -60,5 +60,9 @@ public class PlayingViewController implements BackwardScreen {
         }
     }
 
+    public void openSettingView() throws Exception {
+        ViewController.getSettingView();
+    }
+
 }
 
